76261fd346fe315203d3aaa7533b79368796f4f5
Andres Patiño <andresp199519@gmail.com>
2021-05-20 12:41:44 +0200

    Se eliminaron links inutilizados

--- src/Views/Components/Layout.php
+++ src/Views/Components/Layout.php
@@ -6,12 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
-    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/styles/styles.css">
-    <link rel="stylesheet" href="../../../css/style-card-add-task.css">
-    <link rel="stylesheet" href="../../../css/sb-admin-2.css">
-    <link rel="stylesheet" href="../../../css/sb-admin-2.min.css">
-
+   
     <title>Student Manager</title>
 </head>
