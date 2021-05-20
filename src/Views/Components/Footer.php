ed3bd34d908ba709bc5b9d7173bf212fa33e4476
amrhefny87 <82093081+amrhefny87@users.noreply.github.com>
2021-05-20 16:53:27 +0200

    se hecho el front end de la primera pagina

---
+++ src/Views/Components/Footer.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("Layout.php"); 
+?>
+
+<footer class="sticky-footer text-white">
+    <div class="container">
+        <div class="copyright text-center my-auto">
+            <h4>Copyright &copy; Divers@s</h4>
+        </div>
+    </div>
+</footer>
--- src/Views/Components/Header.php
+++ src/Views/Components/Header.php
@@ -1,3 +1,9 @@
+<?php
+    require_once("Layout.php"); 
+?>
 <header>
-    <h1 class="jumbotron text-center">El Consultorio del Coder</h1>
+    <div class="d-flex p-3">
+        <img src="public/img/logo.png" class="col-lg-1">
+        <h1 class="col-lg-6 px-4 pt-4 text-white">El Consultorio del Coder</h1>
+    </div>
 </header>
